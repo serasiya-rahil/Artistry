b4967d0509a50a4e742f664edb8815d4e778bbb2
serasiya-rahil <serasiya.rahil@gmail.com>
2024-10-10 00:15:37 -0400

    added signup and login for artist

--- ArtistryOnDemand/appln/forms.py
+++ ArtistryOnDemand/appln/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from .models import User as CustomUser
+from .models import Artist, User as CustomUser
 
 class CustomUserSignupForm(UserCreationForm):
     first_name = forms.CharField(max_length=255)
@@ -41,3 +41,43 @@ class CustomUserSignupForm(UserCreationForm):
             custom_user.save()
             
         return user
+
+
+class ArtistSignupForm(forms.ModelForm):
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Confirm Password', widget=forms.PasswordInput)
+
+    class Meta:
+        model = Artist
+        fields = ['first_name', 'last_name', 'username', 'email', 'phone_number']
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Passwords don't match")
+        return password2
+
+    def save(self, commit=True):
+        artist = super().save(commit=False)
+        
+        # Create the User
+        user = User(
+            username=self.cleaned_data['username'],
+            email=self.cleaned_data['email'],
+            first_name=self.cleaned_data['first_name'],
+            last_name=self.cleaned_data['last_name'],
+        )
+        user.set_password(self.cleaned_data['password1'])
+        if commit:
+            user.save()
+
+        # Now save the Artist record
+        artist.password = user.password  # Store the hashed password in the Artist model too
+        if commit:
+            artist.save()
+        return artist
+    
+class ArtistLoginForm(forms.Form):
+    username = forms.CharField(max_length=255)
+    password = forms.CharField(widget=forms.PasswordInput)
